0de9f6b517231f9bf5a28d7df6e12f5149fac54c
Philip Antonov <molo4nik11@gmail.com>
2017-11-02 19:18:03 +0300

    f

--- index.js
+++ index.js
@@ -472,8 +472,6 @@ TRADER.prototype.makeBuyAndSellData = function (next) {
 		return _curr_arr.indexOf(currencyName) == -1 && !self.open_buy_orders_by_curr[el.symbol];
 	});
 
-	console.log('closed_buy_orders_by_curr', self.closed_buy_orders_by_curr);
-
 	self.able_to_sell_pairs = self.available_balances.filter(function (el) {
 		if (self.closed_buy_orders_by_curr[el.currency + '/BTC']) {
 			el.buy_order = self.closed_buy_orders_by_curr[el.currency + '/BTC'].filter(function (_curr) {
