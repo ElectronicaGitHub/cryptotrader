b5ea6a3ed7ecb8b48107f99647bdc4caeefe11d2
Philip Antonov <molo4nik11@gmail.com>
2017-11-09 21:21:21 +0300

    f

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,8 +2,16 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 	console.log('hello');
 
 	let params = {
+		// насколько от нижней границы в процентах заходить на покупку
 		percent_from_min_to_base : 60,
-		percent_graph_raise_value : 1
+		// процент разницы в значениях в начале и в конце графа
+		percent_graph_raise_value : 1,
+		// куда стараемся переставить переставить
+		// от одной границы до другой, неподошло в одну смотрим другую и тд
+		percent_from_base_to_max_to_buy_min : 40,
+		percent_from_base_to_max_to_buy_max : 70,
+		percent_from_base_to_max_step : 10,
+		min_profit_percent : 0.8
 	}
 
 	$scope.setTraderSelected = function(trader) {
@@ -86,7 +94,7 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 				var sums = today_closed_orders.map(function (el) {
 					var b_order = el.buy_order;
 					if (b_order) {
-						return el.price * el.quantity - b_order.price * el.quantity;
+						return el.price * el.quantity - b_order.price * b_order.quantity;
 					} else {
 						return 0;
 					};
@@ -99,6 +107,41 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 			}
 
 		}
+	}
+
+	function makeClosedPairs(orders_currencies) {
+		for (let i in orders_currencies) {
+			let obj = { sell : [], buy : []};
+			for (let k in orders_currencies[i]) {
+				if (orders_currencies[i][k]) {
+					if (orders_currencies[i][k].type == 'LIMIT_BUY') {
+						obj.buy.push(orders_currencies[i][k]);
+					} else if (orders_currencies[i][k].type == 'LIMIT_SELL') {
+						obj.sell.push(orders_currencies[i][k]);
+					}
+				}
+			}
+
+			for (let j in obj.sell) {
+				// закрытые покупки, надо проставить !!!
+				var buys = obj.buy;
+				buys = buys.filter((el) => el.lastModificationTime < obj.sell[j].lastModificationTime);
+				obj.sell[j].buy_order = _.sortBy(buys, 'lastModificationTime')[buys.length - 1];
+			}
+			// delete obj.buy;
+
+			orders_currencies[i] = obj;
+		}
+
+		var orders = [];
+
+		for (let i in orders_currencies) {
+			for (let k in orders_currencies[i].sell) {
+				orders.push(orders_currencies[i].sell[k]);
+			}
+		}
+
+		return orders;
 	}
 
 	$scope.tradeCycle = function () {
@@ -161,41 +204,6 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 		})
 	}
 
-	function makeClosedPairs(orders_currencies) {
-		for (let i in orders_currencies) {
-			let obj = { sell : [], buy : []};
-			for (let k in orders_currencies[i]) {
-				if (orders_currencies[i][k]) {
-					if (orders_currencies[i][k].type == 'LIMIT_BUY') {
-						obj.buy.push(orders_currencies[i][k]);
-					} else if (orders_currencies[i][k].type == 'LIMIT_SELL') {
-						obj.sell.push(orders_currencies[i][k]);
-					}
-				}
-			}
-
-			for (let j in obj.sell) {
-				// закрытые покупки, надо проставить !!!
-				var buys = obj.buy;
-				buys = buys.filter((el) => el.lastModificationTime < obj.sell[j].lastModificationTime);
-				obj.sell[j].buy_order = _.sortBy(buys, 'lastModificationTime')[buys.length - 1];
-			}
-			// delete obj.buy;
-
-			orders_currencies[i] = obj;
-		}
-
-		var orders = [];
-
-		for (let i in orders_currencies) {
-			for (let k in orders_currencies[i].sell) {
-				orders.push(orders_currencies[i].sell[k]);
-			}
-		}
-
-		return orders;
-	}
-
 	$scope.exchange_pairs_filter = function (el) {
 		return el.rank > 0.2;
 	}
@@ -229,20 +237,11 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 
 		if (!data) return;
 
-		// data = data.map((el, n) => [n * 0.0001, el.close, 1]);
-		// data = data.map((el, n) => [n, el.close]);
 		data = data.map((el, n) => [el.timestamp, +el.best_ask, 1]);
+
 		y_min = _.minBy(data, 1);
 		y_max = _.maxBy(data, 1);
-
 		x_max = _.maxBy(data, 0);
-
-		// data = data.map((el, n) => [y_min[1] + (n * (y_max[1] - y_min[1])/data.length), el[1]]);
-
-		// data = data.map(el => [el[0]/x_max[0], el[1]/y_max[1], 1]);
-		// data = data.map(el => [x_max[0] - el[0], y_max[1] - el[1], 1]);
-
-		// console.log(data);
 
 		lines = makeLines(data);
 
@@ -250,15 +249,12 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 		pair.diff_value = (data[0][1] - y_min[1])/y_min[1] * 100;
 		pair.spread_value = (y_max[1] - y_min[1])/y_min[1] * 100;
 		pair.baseline_m = lines.baseLine.m;
-
-		// рассматриваем что последняя точка находится в нижнем коридоре
 		
 		// первое значение
 		let first_value_x = data[0][0];
 		// последнее значение x и y
 		let last_value_x = data[data.length - 1][0];
 		let last_value_y = data[data.length - 1][1];
-		
 		// первое значение y на базовой регрессии
 		let first_base_y = pair.first_base_y = lines.baseLine.m * first_value_x + lines.baseLine.b;
 		let first_min_y = lines.minLine.m * first_value_x + lines.minLine.b;
@@ -272,12 +268,16 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 		// разницы от/до в y
 		let diff_from_min_to_base = last_base_y - last_min_y;
 		let diff_from_min_to_max = last_max_y - last_min_y;
+		let diff_from_base_to_max = last_max_y - last_base_y;
 		// процентная разница нашего значения в промежутке между минимумом и базой
 		let percent_from_min_to_base = (last_value_y - last_min_y)/(diff_from_min_to_base) * 100;
 		// процентная разница нашего значения в промежутке между минимумом и максимумом
 		let percent_from_min_to_max = (last_value_y - last_min_y)/(diff_from_min_to_max) * 100;
 		// получаем наши линии
 		lines = makeLines(data, diff_from_min_to_base);
+
+		// коэффициент роста нашего графа по базовой регрессии
+		pair.percent_graph_raise_value = (last_base_y - first_base_y) / first_base_y * 100;
 
 		// считаем прибыль при покупке в текущий момент
 		let current_ask = +pair.best_ask;
@@ -286,58 +286,83 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 		let price_in_btc = current_ask * quantity; // понимаем цену в битках
 		// let profit_price_in_btc = (price_in_btc * (100 + +trader.exchange.profit_koef) / 100) + tax;
 		// let sell_price = profit_price_in_btc / quantity;
+	
+		// переставляем через добавление низ->средняя
+		// let sell_price = current_ask + diff_from_min_to_base;
+		// 
+		// переставляем через процент средняя->мах
+		// let sell_price_min = current_ask + (diff_from_base_to_max * params.percent_from_base_to_max_to_buy_min / 100);
+		// let sell_price_max = current_ask + (diff_from_base_to_max * params.percent_from_base_to_max_to_buy_max / 100);
+		// 
+		// 
 
-		let sell_price = current_ask + diff_from_min_to_base;
-		let tax = (sell_price * quantity) * (trader.exchange.exchange_fee * 2);
-		let new_price_in_btc = (sell_price + tax) * quantity;
+		// Подготовка данных
 
-		let percent_profit = ((new_price_in_btc - price_in_btc) / price_in_btc * 100).toFixed(3);
+		let percent_profit = 0;
+		// let percent_profit_min = 0;
+		// let percent_profit_max = 0;
+		let current_percent_from_base_to_max = params.percent_from_base_to_max_to_buy_min;
 
-		// console.log(percent_profit);
+		let sell_price, sell_price_min, sell_price_max;
+		let tax, tax_min, tax_max;
+		let new_price_in_btc, new_price_in_btc_min, new_price_in_btc_max;
+		let usd_profit, usd_profit_min, usd_profit_max;
 
-		console.log(trader.exchange.name, pair.symbol, percent_profit);
+		do {
+			sell_price = last_base_y + (last_max_y - last_base_y) * current_percent_from_base_to_max / 100;
+			// sell_price_min = last_base_y + (last_max_y - last_base_y) * params.percent_from_base_to_max_to_buy_min / 100;
+			// sell_price_max = last_base_y + (last_max_y - last_base_y) * params.percent_from_base_to_max_to_buy_max / 100;
+
+			tax = (sell_price * quantity) * (trader.exchange.exchange_fee * 2);
+			// tax_min = (sell_price_min * quantity) * (trader.exchange.exchange_fee * 2);
+			// tax_max = (sell_price_max * quantity) * (trader.exchange.exchange_fee * 2);
+
+			new_price_in_btc = (sell_price + tax) * quantity;
+			// new_price_in_btc_min = (sell_price_min + tax_min) * quantity;
+			// new_price_in_btc_max = (sell_price_max + tax_max) * quantity;
+
+			percent_profit = ((new_price_in_btc - price_in_btc) / price_in_btc * 100);
+			// percent_profit_min = ((new_price_in_btc_min - price_in_btc) / price_in_btc * 100).toFixed(3);
+			// percent_profit_max = ((new_price_in_btc_max - price_in_btc) / price_in_btc * 100).toFixed(3);
+
+			usd_profit = (new_price_in_btc - price_in_btc) * trader.btc_usd.best_ask;
+			// usd_profit_min = (new_price_in_btc_min - price_in_btc) * trader.btc_usd.best_ask;
+			// usd_profit_max = (new_price_in_btc_max - price_in_btc) * trader.btc_usd.best_ask;
+
+			// прибавляем процент перестановки
+			current_percent_from_base_to_max += params.percent_from_base_to_max_step;
+
+		} while (
+			(percent_profit <= params.min_profit_percent) && 
+			(current_percent_from_base_to_max < params.percent_from_base_to_max_to_buy_max)
+		);
+
+
+		console.log(trader.exchange.name, pair.symbol, current_percent_from_base_to_max,
+					percent_profit.toFixed(3) + ' (' + usd_profit.toFixed(3) + ' USD)', 
+					// percent_profit_min + ' (' + usd_profit_min.toFixed(3) + ' USD) -', 
+					// percent_profit_max + ' (' + usd_profit_max.toFixed(3) + ' USD)'
+		);
 
 		// Нашли перестановку, определили процент профита
 		// Если перестановка при текущей покупке лежит в должном проценте от линии
 		// базовой регрессии то покупаем пару и переставляем именно на эту цену
 
+		let exs = [];
+		// текущее значение больше минимума регрессии
+		// exs.push(last_value_y >= last_min_y);
+		// текущее значение меньше базовой регрессии
+		exs.push(last_value_y < last_base_y);
+		// затухание тренда не больше чем коэффициент
+		// exs.push(pair.percent_graph_raise_value > -params.percent_graph_raise_value);
+		// последняя точка выше предыдущей
+		// exs.push(last_value_y > data[data.length - 2][1]);
+		// нахождение в зоне между минимумом и базой
+		// exs.push(percent_from_min_to_base < params.percent_from_min_to_base);
+		// профит больше чем минимально допустимый
+		exs.push(percent_profit > params.min_profit_percent);
 
-
-		// console.log('Покупаем', current_ask);
-		// console.log('Такса', tax);
-
-		// console.log('Продаем', sell_price);
-
-		// console.log(lines);
-		// console.log('y_max[1]', y_max[1]);
-		// console.log('y_min[1]', y_min[1]);
-
-		// console.log('function max value', last_max_y);
-		// console.log('function base value', last_base_y);
-		// console.log('function min value', last_min_y);
-		// console.log('last value', last_value_y);	
-	
-		// console.log('diff_from_min_to_base', diff_from_min_to_base);
-		// console.log('diff_from_min_to_max', diff_from_min_to_max);
-
-		// console.log('percent from min to base', percent_from_min_to_base);
-		// console.log('percent from min to max', percent_from_min_to_max);
-
-		// console.log('percent_graph_raise_value', Math.abs((last_base_y - first_base_y) / first_base_y * 100));
-		let percent_graph_raise_value = (last_base_y - first_base_y) / first_base_y * 100;
-
-
-		if (last_value_y > last_min_y && 
-			last_value_y < last_base_y && 
-			// Math.abs(pair.spread_value) > 3 && // ???
-			percent_graph_raise_value > -params.percent_graph_raise_value &&  
-			last_value_y > data[data.length - 2][1] &&
-			percent_from_min_to_base < params.percent_from_min_to_base) {
-			pair.is_graph_acceptable = true;			
-		}
-
-		// pair.is_graph_acceptable = Math.abs(pair.spread_value) > 3;
-
+		pair.is_graph_acceptable = exs.reduce((a, b) => a && b);
 
 		(function (data, lines) {
 			setTimeout(function () {
@@ -356,7 +381,10 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 				    {
 			            type: 'line',
 			            name: 'Regression Line',
-			            data: [[lines.baseLine.data[0].x, lines.baseLine.data[0].y], [lines.baseLine.data[1].x, lines.baseLine.data[1].y]], 
+			            data: [
+			            	[lines.baseLine.data[0].x, lines.baseLine.data[0].y], 
+			            	[lines.baseLine.data[1].x, lines.baseLine.data[1].y]
+			            ], 
 			            marker: { enabled: false },
 			            states: { hover: { lineWidth: 0 } },
 			            enableMouseTracking: false
@@ -364,8 +392,10 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 			        {
 			            type: 'line',
 			            name: 'Regression Line Min',
-			            data: [[lines.minLine.data[0].x, lines.minLine.data[0].y], [lines.minLine.data[1].x, lines.minLine.data[1].y]], 
-			            // data: [[test_x0, test_y0], [test_xf, test_yf]], 
+			            data: [
+			            	[lines.minLine.data[0].x, lines.minLine.data[0].y], 
+			            	[lines.minLine.data[1].x, lines.minLine.data[1].y]
+			            ], 
 			            marker: { enabled: false },
 			            states: { hover: { lineWidth: 0 } },
 			            enableMouseTracking: false,
@@ -375,8 +405,10 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 			        {
 			            type: 'line',
 			            name: 'Regression Line Max',
-			            data: [[lines.maxLine.data[0].x, lines.maxLine.data[0].y], [lines.maxLine.data[1].x, lines.maxLine.data[1].y]], 
-			            // data: [[max_test_x0, max_test_y0], [max_test_xf, max_test_yf]], 
+			            data: [
+			            	[lines.maxLine.data[0].x, lines.maxLine.data[0].y], 
+			            	[lines.maxLine.data[1].x, lines.maxLine.data[1].y]
+			            ], 
 			            marker: { enabled: false },
 			            states: { hover: { lineWidth: 0 } },
 			            enableMouseTracking: false,
@@ -386,8 +418,10 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 					{
 			            type: 'line',
 			            name: 'Regression Line Min Percentage',
-			            data: [[lines.minPercentageLine.data[0].x, lines.minPercentageLine.data[0].y], [lines.minPercentageLine.data[1].x, lines.minPercentageLine.data[1].y]], 
-			            // data: [[max_test_x0, max_test_y0], [max_test_xf, max_test_yf]], 
+			            data: [
+			            	[lines.minPercentageLine.data[0].x, lines.minPercentageLine.data[0].y], 
+			            	[lines.minPercentageLine.data[1].x, lines.minPercentageLine.data[1].y]
+			            ], 
 			            marker: { enabled: false },
 			            states: { hover: { lineWidth: 0 } },
 			            enableMouseTracking: false,
@@ -407,6 +441,38 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 			            color : '#FF0000',
 			            lineWidth: 3
 			        },
+			        // {
+			        //     type: 'line',
+			        //     name: 'Линия перестановки макс',
+			        //     data: [
+			        //     	[lines.baseLine.data[0].x, sell_price_max], 
+			        //     	[lines.baseLine.data[1].x, sell_price_max]
+			        //     ], 
+			        //     marker: { enabled: false },
+			        //     states: { hover: { lineWidth: 0 } },
+			        //     enableMouseTracking: false,
+			        //     color : '#FF0000',
+			        //     lineWidth: 3
+			        // },
+
+			    //     {
+				   //      name: 'Range Перестановки',
+				   //      data: [
+							// [ first_value_x, sell_price_min, sell_price_max],
+							// [ last_value_x, sell_price_min, sell_price_max]
+				   //      ],
+				   //      type: 'arearange',
+				   //      lineWidth: 0,
+				   //      linkedTo: ':previous',
+				   //      states: { hover: { lineWidth: 0 } },
+				   //      color: '#FF0000',
+				   //      fillOpacity: 0.5,
+				   //      zIndex: 0,
+				   //      marker: {
+				   //          enabled: false
+				   //      }
+			    //     },
+
 			        {
 				        name: 'Full Range',
 				        data: [
@@ -423,7 +489,6 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 				            enabled: false
 				        }
 			        },
-			        // линия перестановки при текущей покупке
 			        { 
 				    	type: 'line', 
 				    	marker: { enabled: false },
@@ -438,53 +503,32 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 
 	let makeLines = (data, diff_from_min_to_base) => {
 
-		var slope = regression('linear', data);
-		// var slope = linear_regression(data);
+		let slope = regression('linear', data);
 
-		// console.log(slope);
-
-		var m = slope.equation[0], b = slope.equation[1];
-		// var m = slope[0], b = slope[1];
-		var xs = [], ys = [];
-
-		// console.log('|arctan(' +  m  + ')| =', Math.abs(Math.atan(m)));
-		// console.log('|arctan(' +  m  + ')| < 0.17', Math.abs(Math.atan(m)) < 0.17);
-
-		// console.log(slope);
+		let m = slope.equation[0], b = slope.equation[1];
+		let xs = [], ys = [];
 
 		data.forEach(function(d){
 		   	xs.push(d[0]);
 		    ys.push(d[1]);
 		});
 
-
-
-		var diffData = data.map(([x,y]) => {
+		let diffData = data.map(([x,y]) => {
 			return [x, y - (m*x + b)]
 		});
 
-		// console.log(diffData);
+		let min = _.minBy(diffData, 1);
+		let max = _.maxBy(diffData, 1);
 
-		var min = _.minBy(diffData, 1);
-		var max = _.maxBy(diffData, 1);
-
-		// console.log('min diff', min);
-		// console.log('max diff', max);
-
-	    // console.log('x0', x0);
-	    // console.log('y0', y0);
-	    // console.log('xf', xf);
-	    // console.log('yf', yf);
-
-		var x0 = Math.min.apply(null, xs), 
+		let x0 = Math.min.apply(null, xs), 
 		    y0 = m*x0 + b;
-		var xf = Math.max.apply(null, xs), 
+		let xf = Math.max.apply(null, xs), 
 		    yf = m*xf + b;
 
-	    var y0max = y0 + max[1];
-	    var y0min = y0 + min[1];
-		var yfmax = yf + max[1];
-	    var yfmin = yf + min[1];
+	    let y0max = y0 + max[1];
+	    let y0min = y0 + min[1];
+		let yfmax = yf + max[1];
+	    let yfmin = yf + min[1];
 
 	    let addition = diff_from_min_to_base * params.percent_from_min_to_base / 100;
 
@@ -518,40 +562,6 @@ angular.module('crypto', []).controller('main', ['$scope', '$http', function($sc
 			minPercentageLine : {data : minPercentageLine, m : minPercentageEquation.m, b : minPercentageEquation.b }
 		}
 	}
-
-	let linear_regression = ( xyr ) => {
-	    var i, 
-	        x, y, r,
-	        sumx=0, sumy=0, sumx2=0, sumy2=0, sumxy=0, sumr=0,
-	        a, b;
-
-	    for(i=0;i<xyr.length;i++) {   
-	        // this is our data pair
-	        x = xyr[i][0]; y = xyr[i][1]; 
-
-	        // this is the weight for that pair
-	        // set to 1 (and simplify code accordingly, ie, sumr becomes xy.length) if weighting is not needed
-	        r = xyr[i][2];  
-
-	        // consider checking for NaN in the x, y and r variables here 
-	        // (add a continue statement in that case)
-
-	        sumr += r;
-	        sumx += r*x;
-	        sumx2 += r*(x*x);
-	        sumy += r*y;
-	        sumy2 += r*(y*y);
-	        sumxy += r*(x*y);
-	    }
-
-	    // note: the denominator is the variance of the random variable X
-	    // the only case when it is 0 is the degenerate case X==constant
-	    b = (sumy*sumx2 - sumx*sumxy)/(sumr*sumx2-sumx*sumx);
-	    a = (sumr*sumxy - sumx*sumy)/(sumr*sumx2-sumx*sumx);
-
-	    return [a, b];
-	}
-
 
 	// делалось для какого то хуя
 	$scope.makeChart = function(trader) {
