fc18ea5a84a7f1d80c1a5f4e6f22b88b4145f068
Philip Antonov <molo4nik11@gmail.com>
2017-11-12 05:24:36 +0300

    v2.1

--- index.js
+++ index.js
@@ -715,7 +715,11 @@ TRADER.prototype.sellPair = function (currency, quantity, buy_order, quick_sell,
 		if (reason == 'profit_sell') {
 			sell_price = buy_order.analyticsResult.values.sell_price;
 		} else if (reason == 'stop_loss') {
-			sell_price = buy_order.analyticsResult.values.stop_loss_price;
+			if (buy_order.analyticsResult.values.stop_loss_price) {
+				sell_price = buy_order.analyticsResult.values.stop_loss_price;
+			} else {
+				sell_price = self.calculateSellPrice(currency, buy_order, quantity, quick_sell);
+			}
 		}
 	} else {
 		console.log('Ордер не содержит данных аналитики');
